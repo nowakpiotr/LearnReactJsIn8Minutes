b501e2f30f1d179badc043176e027dc5e8043cf5
Piotr Nowak <piotrek.nowak@outlook.com>
2016-08-23 12:52:15 +0200

    Setting Up a React.js Environment Using Npm, Babel 6 and Webpack

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import AwesomeComponent from './AwesomeComponent.jsx';
+
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return <div><p> Hello React!</p><AwesomeComponent/></div>;
   }
 }
 
